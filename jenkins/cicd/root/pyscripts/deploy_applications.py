04247f18fedfb9aaa1c180aecfac75ec1e8aae17
HakimiX <mustafa.hakimi@outlook.dk>
2022-06-05 20:58:22 +0200

    debug jenkins context

--- jenkins/cicd/root/pyscripts/deploy_applications.py
+++ jenkins/cicd/root/pyscripts/deploy_applications.py
@@ -5,7 +5,8 @@ from environments import environment_configs
 
 def change_kubernetes_context(env):
     print('Changing Kubernetes Context')
-    cmd = 'kubectl config use-context {}'.format(environment_configs[env].kubernetes_context)
+    #cmd = 'kubectl config use-context {}'.format(environment_configs[env].kubernetes_context)
+    cmd = 'kubectl config get-contexts'
     cmd_result = os.system(cmd)
     if cmd_result != 0:
         print('Failed to change kubernetes context')
