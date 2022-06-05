eb70d82d241c75ad8c541f92fbd96c15d6b00241
HakimiX <mustafa.hakimi@outlook.dk>
2022-06-05 19:55:40 +0200

    move initialize logic to the correct py file

--- jenkins/cicd/root/pyscripts/deploy_applications.py
+++ jenkins/cicd/root/pyscripts/deploy_applications.py
@@ -1,1 +1,20 @@
-print('python script works')
+import os
+
+
+def initialize():
+    """Initialize parameters and environment variables"""
+    env_vars = ['PROJECTS', 'ENVIRONMENT', 'BRANCH']
+    missing = set(env_vars) - set(os.environ)
+    if missing:
+        print('Environment variables do not exist: %s' % missing)
+    projects = os.getenv('PROJECTS')
+    env = os.getenv('ENVIRONMENT')
+    branch = os.getenv('BRANCH')
+
+    print('''
+    Environment variables
+    Projects: %s, Environment: %s, Branch: %s 
+    '''.format(projects, env, branch))
+
+
+initialize()
