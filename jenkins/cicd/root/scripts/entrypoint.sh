c6d49ef6e52204a6f1113f3e6b6fdbe33e5bb358
HakimiX <mustafa.hakimi@outlook.dk>
2022-06-07 17:56:55 +0200

    .

--- jenkins/cicd/root/scripts/entrypoint.sh
+++ jenkins/cicd/root/scripts/entrypoint.sh
@@ -9,11 +9,11 @@ ls -a
 #echo "content of kube directory"
 #ls .kube
 
-#echo "content of .kube/config"
-#cat .kube/config
+echo "content of .kube/config"
+cat .kube/config
 
-echo "content of client dir"
-ls -a client
+#echo "content of client dir"
+#ls -a client
 
 echo "environment variables"
 env
