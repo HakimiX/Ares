40ed367ddcc8089ed13e7b0109cc14d5ba06c633
HakimiX <mustafa.hakimi@outlook.dk>
2022-06-07 19:07:55 +0200

    .

--- jenkins/cicd/root/pyscripts/deploy_applications.py
+++ jenkins/cicd/root/pyscripts/deploy_applications.py
@@ -6,20 +6,13 @@ from environments import environment_configs
 def change_kubernetes_context(env):
     print('Changing Kubernetes Context')
     #cmd = 'kubectl config use-context {}'.format(environment_configs[env].kubernetes_context)
-    #cmd = 'kubectl config get-contexts'
+    cmd = 'kubectl config get-contexts'
 
-    cmd_1 = 'cd ../../'
-    os.system(cmd_1)
-
-    print('Current dir')
-    cmd_2 = 'pwd'
-    os.system(cmd_2)
-
-    #cmd = 'kubectl config current-context'
-    #cmd_result = os.system(cmd)
-    #if cmd_result != 0:
-    #    print('Failed to change kubernetes context')
-    #    sys.exit(1)
+    cmd = 'kubectl config current-context'
+    cmd_result = os.system(cmd)
+    if cmd_result != 0:
+        print('Failed to change kubernetes context')
+        sys.exit(1)
 
 
 def handler(projects, env, branch):
